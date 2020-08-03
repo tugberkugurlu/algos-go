9433a19d220011d339e2b9c77855516cd48f91e9
Tugberk Ugurlu <tugberk@outlook.com>
2020-08-03 13:14:22 +0100

    add kth largest solution

---
+++ kth-largest/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"container/heap"
+	"fmt"
+)
+
+type IntHeap []int
+
+func (h IntHeap) Len() int {
+	return len(h)
+}
+
+func (h IntHeap) Less(i, j int) bool {
+	return h[i] < h[j]
+}
+
+func (h IntHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0:n-1]
+	return x
+}
+
+func main() {
+	fmt.Println(FindLargestKthElement([]int{3,2,3,1,2,4,5,5,6}, 4))
+}
+
+// O(N log K + N-K log K)
+func FindLargestKthElement(nums []int, k int) int {
+	h := &IntHeap{}
+	for _, val := range nums { // O(N)
+		heap.Push(h, val) // O(log K)
+		if h.Len() > k {
+			heap.Pop(h) // O(log K)
+		}
+	}
+
+	return heap.Pop(h).(int) // O(log K)
+}
