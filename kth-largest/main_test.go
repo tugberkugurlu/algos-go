2ac849257816a95d4560c68fc936186a5eaf0c0a
Tugberk Ugurlu <tugberk@outlook.com>
2020-08-03 21:46:24 +0100

    add tests for kth largest element logic

--- kth-largest/main_test.go
+++ kth-largest/main_test.go
@@ -1,7 +1,26 @@
-package main_test
+package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+var testdata = []struct {
+	in  []int
+	k   int
+	out int
+}{
+	{[]int{3, 2, 1, 5, 6, 4}, 2, 5},
+	{[]int{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4, 4},
+}
 
 func TestLogic(t *testing.T) {
-
+	for _, tt := range testdata {
+		t.Run(fmt.Sprintf("%v", tt.in), func(t *testing.T) {
+			out := FindLargestKthElement(tt.in, tt.k)
+			if out != tt.out {
+				t.Errorf("got %q, want %q", out, tt.out)
+			}
+		})
+	}
 }
